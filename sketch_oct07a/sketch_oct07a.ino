d13cf931ec6690622b97a882ab0f59a01d877f50
Unknown <jdso@hotmail.com>
2018-10-09 20:46:03 +0100

    Code cleanse.

--- sketch_oct07a/sketch_oct07a.ino
+++ sketch_oct07a/sketch_oct07a.ino
@@ -9,11 +9,8 @@ GameScreens prev_state;
 // ---- Functions that program will need ----
 
 void random_blinking() {
-  while(true) {
-    for(int i=2; i<=6; i++) {
-      digitalWrite(i, random(0,2));
-    }
-    delay(65);
+  for(int i=2; i<=6; i++) {
+    digitalWrite(i, random(0,2));
   }
 }
 
@@ -68,13 +65,13 @@ void setup() {
   // ---- Code to run once, on start ----
 
   // Define pins A0-A4 as inputs and pins 2-6 as outputs
-  
+
   pinMode(A0, INPUT);
   pinMode(A1, INPUT);
   pinMode(A2, INPUT);
   pinMode(A3, INPUT);
   pinMode(A4, INPUT);
-  
+
   pinMode(2, OUTPUT);
   pinMode(3, OUTPUT);
   pinMode(4, OUTPUT);
@@ -82,21 +79,29 @@ void setup() {
   pinMode(6, OUTPUT);
 
   main_state = START;
-
+  prev_state = main_state;
 }
 
 void loop() {
   switch(main_state) {
     case START:
+      if(prev_state!=START) sequential_blinking_current_stage=0;
       sequential_blinking();
-      delay(500); // ESTE DELAY devia ser partido pq o estado pode mudar
+      prev_state = START;
+      for (int i=0; i<10 && main_state==START; i++) { delay(50); } // 10x50ms delay
       break;
     case GAME:
+
+      prev_state = GAME;
       break;
     case LOSE:
+
+      prev_state = LOSE;
       break;
     case WIN:
       random_blinking();
+      prev_state = WIN;
+      delay(65);
       break;
     default:
       digitalWrite(2, HIGH);
@@ -104,181 +109,7 @@ void loop() {
       digitalWrite(4, HIGH);
       digitalWrite(5, HIGH);
       digitalWrite(6, HIGH);
+      prev_state = ERROR;
   }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-const int MAX_LEVEL = 100;
-int sequence[MAX_LEVEL];
-int your_sequence[MAX_LEVEL];
-
-int velocity = 1000;
-
-void loop()
-{
-if (level == 1)
-generate_sequence();//generate a sequence;
-
-if (digitalRead(A4) == LOW || level != 1) //If start button is pressed or you're winning
-{
-show_sequence();    //show the sequence
-get_sequence();     //wait for your sequence
-}
-}
-
-void show_sequence()
-{
-digitalWrite(2, LOW);
-digitalWrite(3, LOW);
-digitalWrite(4, LOW);
-digitalWrite(5, LOW);
-
-for (int i = 0; i < level; i++)
-{
-digitalWrite(sequence[i], HIGH);
-delay(velocity);
-digitalWrite(sequence[i], LOW);
-delay(200);
-}
-}
-
-void get_sequence()
-{
-int flag = 0; //this flag indicates if the sequence is correct
-
-for (int i = 0; i < level; i++)
-{
-flag = 0;
-while(flag == 0)
-{
-if (digitalRead(A0) == LOW)
-{
-digitalWrite(5, HIGH);
-your_sequence[i] = 5;
-flag = 1;
-delay(200);
-if (your_sequence[i] != sequence[i])
-{
-wrong_sequence();
-return;
-}
-digitalWrite(5, LOW);
-}
-
-if (digitalRead(A1) == LOW)
-{
-digitalWrite(4, HIGH);
-your_sequence[i] = 4;
-flag = 1;
-delay(200);
-if (your_sequence[i] != sequence[i])
-{
-wrong_sequence();
-return;
-}
-digitalWrite(4, LOW);
-}
-
-if (digitalRead(A2) == LOW)
-{
-digitalWrite(3, HIGH);
-your_sequence[i] = 3;
-flag = 1;
-delay(200);
-if (your_sequence[i] != sequence[i])
-{
-wrong_sequence();
-return;
-}
-digitalWrite(3, LOW);
-}
-
-if (digitalRead(A3) == LOW)
-{
-digitalWrite(2, HIGH);
-your_sequence[i] = 2;
-flag = 1;
-delay(200);
-if (your_sequence[i] != sequence[i])
-{
-wrong_sequence();
-return;
-}
-digitalWrite(2, LOW);
-}
-
-}
-}
-right_sequence();
-}
-
-void generate_sequence()
-{
-randomSeed(millis()); //in this way is really random!!!
-
-for (int i = 0; i < MAX_LEVEL; i++)
-{
-sequence[i] = random(2,6);
-}
-}
-void wrong_sequence()
-{
-for (int i = 0; i < 3; i++)
-{
-digitalWrite(2, HIGH);
-digitalWrite(3, HIGH);
-digitalWrite(4, HIGH);
-digitalWrite(5, HIGH);
-delay(250);
-digitalWrite(2, LOW);
-digitalWrite(3, LOW);
-digitalWrite(4, LOW);
-digitalWrite(5, LOW);
-delay(250);
-}
-level = 1;
-velocity = 1000;
-}
-
-void right_sequence()
-{
-digitalWrite(2, LOW);
-digitalWrite(3, LOW);
-digitalWrite(4, LOW);
-digitalWrite(5, LOW);
-delay(250);
-
-digitalWrite(2, HIGH);
-digitalWrite(3, HIGH);
-digitalWrite(4, HIGH);
-digitalWrite(5, HIGH);
-delay(500);
-digitalWrite(2, LOW);
-digitalWrite(3, LOW);
-digitalWrite(4, LOW);
-digitalWrite(5, LOW);
-delay(500);
-
-if (level < MAX_LEVEL);
-level++;
-
-velocity -= 50; //increase difficulty
-}
-
-*/
